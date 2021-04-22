98a1272d1fe321d3a5a59f3d36eda88ce0ec3b27
jamescross <crossjames813@gmail.com>
2021-04-22 15:02:06 -0500

    commit tree 05f8449e6f9f29b55b13a11bf4a1567ceba9c081

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    # binding.pry
+    @article = Article.create(params)
+    redirect "articles/#{@article.id}"
+  end
+  
+  get '/articles/:id' do
+    # binding.pry
+    @article = Article.find(id = params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    # binding.pry
+
+    @article = Article.find(id = params[:id])
+    # binding.pry
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    
+    redirect "articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect to "/articles"
+  end
 end
